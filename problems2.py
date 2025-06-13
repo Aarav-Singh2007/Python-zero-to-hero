d50ff6dfc4fee7ba9ea3a217759c052d9f1043d8
Aarav-Singh2007 <aaravsingh7july2007@gmail.com>
2025-06-13 21:35:32 +0530

    Conditional related statements

---
+++ problems2.py
@@ -0,0 +1,13 @@
+#part 2 here i will be adding problems related to conditional statements
+#Colour of the traffic light
+light = input("light : ")
+if (light == 'Red'):
+    print("stop")
+elif(light == "Green"):
+   
+    print("Go")
+elif(light == "Yellow"):
+  
+    print("Wait")
+else:
+    print("light us broken")
